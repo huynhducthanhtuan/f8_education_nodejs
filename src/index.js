a858bc7646050f3797e067d716878d9e8fd04a42
huynhdthanhtuan <tuan40655@gmail.com>
2022-02-18 16:33:56 +0700

    try to deploy to Heroku 4

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Import libraries
 const express = require('express');
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const path = require('path');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
