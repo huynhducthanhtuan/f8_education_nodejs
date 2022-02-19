9f0fc3b9518a6f45cd54e148a5455f1502007c1e
huynhdthanhtuan <tuan40655@gmail.com>
2022-02-19 10:51:40 +0700

    add env

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 // Import libraries
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000;
+require('dotenv').config();
+const port = process.env.PORT || 3000;
 const path = require('path');
 const morgan = require('morgan');
 const handlebars = require('express-handlebars');
