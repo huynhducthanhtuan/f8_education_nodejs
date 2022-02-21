6f251f9f859b194e4a8dba29a0e22f8e4fee7ee5
huynhdthanhtuan <tuan40655@gmail.com>
2022-02-21 19:22:58 +0700

    can giua noi dung cac trang search, create-course, edit-course, login

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,12 +1,8 @@
-const Course = require('../../app/models/Course.js');
 const mongoose = require('mongoose');
-// const mongouri = "mongodb://localhost:27017/f8_education_nodejs";
-const mongouri =
-    'mongodb+srv://zyxnone120:SAKHFWEKJWE@f8-education.i9nau.mongodb.net/f8_education_nodejs?retryWrites=true&w=majority';
 
 async function connect() {
     try {
-        mongoose.connect(mongouri, {useNewUrlParser: true});
+        mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
         mongoose.connection.on('error', (error) => console.log(error));
         mongoose.connection.on('open', () => {
             console.log('Connected to MongoDB database.');
