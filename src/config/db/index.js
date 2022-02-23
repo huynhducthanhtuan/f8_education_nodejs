cf9b424df4e728f4cb34f158ad8f16c9fdfd6b0a
huynhdthanhtuan <tuan40655@gmail.com>
2022-02-24 00:13:36 +0700

    fix bug: deploy to heroku doneee

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
-const MONGODB_URI = require('./mongouri');
 
 async function connect() {
     try {
-        mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+        mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
         mongoose.connection.on('error', (error) => console.log(error));
         mongoose.connection.on('open', () => {
             console.log('Connected to MongoDB database.');
